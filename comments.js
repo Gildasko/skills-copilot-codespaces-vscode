8953b45ba8670ac9004ac439ebcedad978f810a9
Gildasko <kcgildas@gmail.com>
2023-08-10 14:07:21 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,21 +1,7 @@
-// Create web server with node.js
+// Create web server 
 
-// 1. import module
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const qs = require('querystring');
-
-// 2. create server
-const app = http.createServer((request, response) => {
-  const _url = request.url;
-  const queryData = url.parse(_url, true).query;
-  const pathname = url.parse(_url, true).pathname;
-  
-  // 3. get data
-  if (pathname === '/') {
-    if (queryData.id === undefined) {
-      fs.readdir('./data', (err, files) => {
-        const title = 'Welcome';
-        const description = 'Hello, Node.js';
-        const list = `<ul>${files.map(file => `<li><a href="/?id=${file}">${file}</a></li>`).join('')
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const Comment = require('../../models/Comment');
+const Post = require('../../models/Post');
